8c7cb8b2637ebc30a1ada1e61969d5c5c707e1b1
Daniel Rebelsky <drebelsky@gmail.com>
2020-07-07 16:01:20 -0500

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: __dirname,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-react-jsx']
+          }
+        }
+      }
+    ]
+  }
+};
